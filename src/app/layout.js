9fa6a00bf9b50032c31669f7c78c5e96299766d5
Shembou <92935278+Shembou@users.noreply.github.com>
2023-09-25 20:46:44 +0200

    added noindex

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="robots" content="noindex"></meta>
+      </head>
         <body className={inter.className}>{children}</body>
     </html>
   );
