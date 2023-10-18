cc75c2dd09388fd1425461e03a28a6e427cf7d73
Shembou <92935278+Shembou@users.noreply.github.com>
2023-10-18 13:00:09 +0200

    Added uxDesign component

---
+++ src/app/pl/projektowanie-ux/page.js
@@ -0,0 +1,41 @@
+import fetchData from "@/utils/fetchData";
+import SEO from "@/components/global/Seo";
+import Hero from "@/app/components/sections/UxDesign/Hero";
+import UxExplanation from "@/app/components/sections/UxDesign/UxComponent";
+import styles from "./styles.module.scss";
+
+export async function generateMetadata() {
+	const {
+		page: { seo },
+	} = await getUxDesignPageData();
+	return SEO({
+		title: seo?.title,
+		description: seo?.description,
+		url: "",
+	});
+}
+
+export default function UxDesignPage() {
+	return (
+		<main id="main" className={styles.main}>
+			<Hero></Hero>
+			<UxExplanation></UxExplanation>
+		</main>
+	);
+}
+
+//wait for new database data
+const getUxDesignPageData = async () => {
+	const {
+		body: { data },
+	} = await fetchData(`
+  page: Homepage(id: "homepage") {
+  # SEO
+  seo {
+    title
+    description
+  }
+}
+  `);
+	return data;
+};
