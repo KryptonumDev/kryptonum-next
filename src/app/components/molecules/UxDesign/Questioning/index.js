6a6af96fe0ff192bb8d68ff32db9029ea17e6731
Shembou <92935278+Shembou@users.noreply.github.com>
2023-10-20 14:24:55 +0200

    made Questioning reusable

--- src/app/components/molecules/UxDesign/Questioning/index.js
+++ src/app/components/molecules/UxDesign/Questioning/index.js
@@ -1,26 +1,23 @@
-import DecorativeHeading from '@/app/components/atoms/DecorativeHeading';
-import styles from './styles.module.scss';
+import DecorativeHeading from "@/app/components/atoms/DecorativeHeading";
+import styles from "./styles.module.scss";
 
-const title="Wiesz, jakie dwie litery sprawią, że pomiędzy **Twoją stroną a użytkownikiem** coś naprawdę zaklika? UX."
-const content1="Bez dobrego UX strona internetowa jest jak [ szpilki Louboutina ] pod Giewontem. Ładne, ale nie zabiorą Cię na szczyt."
-const content2="Zaprojektujemy Twoją stronę www, sklep internetowy czy apkę tak, by zachwycała nie tylko designem, ale i użytecznością, dzięki czemu Twoja marka zyska +10 punktów do za… zaufania, of course."
-
-const Questioning = ({className}) => {
+const Questioning = ({ className, question, headingContext, description, questionClassName }) => {
 	return (
 		<>
-		<div className={className}>
-			<div className={styles.decorativeHeadingWrapper}>
-				<DecorativeHeading type="h4">{title}</DecorativeHeading>
-			</div>
-			<div className={styles.contentWrapper}>
-				<div className={styles.question}>
-					<span>Dlaczego?</span>
+			<div className={className}>
+				<div className={styles.decorativeHeadingWrapper}>
+					<DecorativeHeading type="h4">{headingContext}</DecorativeHeading>
 				</div>
-				<div className={styles.explanation}>
-					<p className={styles.firstContent}>{content1}</p>
-					<p>{content2}</p>
+				<div className={styles.contentWrapper}>
+					<div className={questionClassName}>{question && <span>{question}</span>}</div>
+					<div className={styles.explanation}>
+						{description.map((paragraph, index) => (
+							<p className={styles.paragraph} key={index}>
+								{paragraph}
+							</p>
+						))}
+					</div>
 				</div>
-			</div>
 			</div>
 		</>
 	);
--- src/app/components/sections/UxDesign/Plane/index.js
+++ src/app/components/sections/UxDesign/Plane/index.js
@@ -19,18 +19,31 @@ const Plane = () => {
 			},
 		},
 	};
+	const question = "Dlaczego?";
+	const questionHeadingContext =
+		"Wiesz, jakie dwie litery sprawią, że pomiędzy **Twoją stroną a użytkownikiem** coś naprawdę zaklika? UX.";
+	const description = [
+		"Bez dobrego UX strona internetowa jest jak [ szpilki Louboutina ] pod Giewontem. Ładne, ale nie zabiorą Cię na szczyt.",
+		"Zaprojektujemy Twoją stronę www, sklep internetowy czy apkę tak, by zachwycała nie tylko designem, ale i użytecznością, dzięki czemu Twoja marka zyska +10 punktów do za… zaufania, of course.",
+	];
 
 	return (
 		<section className={styles.wrapper}>
-			<Questioning className={styles.questioning} />
+			<Questioning
+				className={styles.questioning}
+				question={question}
+				headingContext={questionHeadingContext}
+				description={description}
+				questionClassName={styles.question}
+			/>
 			<Consultation
-				className={styles.consultation}
+				className={styles.wrapper}
 				image={image}
 				buttonContext={buttonContext}
 				buttonMobileContext={buttonMobileContext}
 				headingContext={headingContext}
-        imageWrapperClassName={styles.imageWrapper}
-        imageClassName={styles.image}
+				imageWrapperClassName={styles.imageWrapper}
+				imageClassName={styles.image}
 			/>
 			<MultipleUxEplanations className={styles.multipleUxEplanations} />
 		</section>
