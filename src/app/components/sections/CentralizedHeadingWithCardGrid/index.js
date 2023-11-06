b85357d8c7ab47034db4cd1e45206e775a7dcda0
Shembou <92935278+Shembou@users.noreply.github.com>
2023-11-06 18:03:56 +0100

    added decoration prop to heading

--- src/app/components/sections/CentralizedHeadingWithCardGrid/index.js
+++ src/app/components/sections/CentralizedHeadingWithCardGrid/index.js
@@ -1,14 +1,18 @@
 import CentralizedHeading from "@/app/components/molecules/CentralizedHeading";
 import CardGrid from "@/app/components/organisms/CardGrid";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-const CentralizedHeadingWithCardGrid = ({cardData, headingData}) => {
+const CentralizedHeadingWithCardGrid = ({ cardData, headingData, decoration }) => {
 	return (
-    <section className={styles.section}>
-			<CentralizedHeading data={headingData} additionalStyles={styles}></CentralizedHeading>
-			<CardGrid data={cardData}/>
+		<section className={styles.section}>
+			<CentralizedHeading
+				data={headingData}
+				decoration={decoration}
+				additionalStyles={styles}
+			></CentralizedHeading>
+			<CardGrid data={cardData} />
 		</section>
-	)
+	);
 };
 
 export default CentralizedHeadingWithCardGrid;
