0ab5bc5d22bf67160a0057ecae73b8e19fcc53c4
Shembou <92935278+Shembou@users.noreply.github.com>
2023-11-15 08:54:27 +0100

    added academy site

---
+++ src/app/components/sections/CuriosityEntries/index.js
@@ -0,0 +1,29 @@
+import DecorativeHeading from "../../atoms/DecorativeHeading";
+import Pagination from "../../organisms/Pagination";
+import styles from "./styles.module.scss";
+import CuriosityEntry from "../../organisms/CuriosityEntry";
+
+const CuriosityEntries = ({ page, totalCount, urlBasis, curiosityEntries, heading }) => {
+	return (
+		<section className={styles.section}>
+			<DecorativeHeading type="h2">
+				{heading || `Arena **ciekawostek** (${curiosityEntries.length})`}
+			</DecorativeHeading>
+			<div className="wrapper">
+				{curiosityEntries.map((entry, i) => (
+					<CuriosityEntry
+						data={entry}
+						key={i}
+					/>
+				))}
+			</div>
+			<Pagination
+				currentPage={page}
+				itemCount={totalCount}
+				urlBasis={urlBasis}
+			/>
+		</section>
+	);
+};
+
+export default CuriosityEntries;
---
+++ src/app/pl/akademia/page.js
@@ -0,0 +1,133 @@
+import { itemsPerPage } from "@/constants/shared";
+import fetchData from "@/utils/fetchData";
+import Hero from "@/app/components/sections/Hero";
+import Categories from "@/app/components/sections/Categories";
+import CtaSection from "@/app/components/sections/CtaSection";
+import Faq from "@/app/components/sections/Faq";
+import CuriosityEntries from "@/app/components/sections/CuriosityEntries";
+import LatestBlogEntries from "@/app/components/sections/homepage/LatestBlogEntries";
+
+export default async function academyPage() {
+	const {
+		page: { hero_Heading, hero_Paragraph, hero_Img, ctaSection },
+		curiosityCategories,
+		curiosityEntries,
+    curiosityEntriesCount
+	} = await query();
+	return (
+		<>
+			<Hero
+				data={{
+					heading: hero_Heading,
+					paragraph: hero_Paragraph,
+					sideImage: hero_Img,
+				}}
+				isBlogHero={true}
+			/>
+			<Categories
+				categorySlug="/pl/akademia/"
+				categories={curiosityCategories}
+			/>
+			<CuriosityEntries
+				urlBasis="/pl/akademia"
+				totalCount={curiosityEntriesCount.length}
+				page={1}
+				curiosityEntries={curiosityEntries}
+			/>
+			<CtaSection data={ctaSection} />
+			<LatestBlogEntries />
+			<Faq />
+		</>
+	);
+}
+
+const query = async () => {
+	const {
+		body: { data },
+	} = await fetchData(`
+  curiosityEntries: allCuriosityEntries(
+    limit: ${itemsPerPage},
+    sort: { _createdAt: DESC }) {
+    title
+    subtitle
+    slug {
+      current
+    }
+    categories {
+      name
+      slug {
+        current
+      }
+    }
+    img {
+      asset {
+        altText
+        url
+          metadata {
+            lqip
+            dimensions {
+              height
+              width
+            }
+          }
+      }
+    }
+    _createdAt
+  }
+  page: Academy(id: "academy") {
+    # Hero
+    hero_Heading
+    hero_Paragraph
+    hero_Img {
+      asset {
+        altText
+        url
+          metadata {
+            lqip
+            dimensions {
+              height
+              width
+            }
+          }
+      }
+    }
+    # Call To Action
+    ctaSection {
+      heading
+      cta {
+        theme
+        text
+        href
+      }
+      img {
+        asset {
+          altText
+          url
+          metadata {
+            lqip
+            dimensions {
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+    # SEO
+    seo {
+      title
+      description
+    }
+  }
+  curiosityCategories: allCuriosityCategories {
+    name
+    slug {
+      current
+    }
+  }
+  curiosityEntriesCount: allCuriosityEntries {
+    _type
+  }
+  `);
+	return data;
+};
