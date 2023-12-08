c383a3446768fe61578c4162942cfdd72f383863
Shembou <92935278+Shembou@users.noreply.github.com>
2023-12-08 11:29:26 +0100

    added default alt text to Img util

--- src/utils/Img.js
+++ src/utils/Img.js
@@ -1,12 +1,9 @@
 import NextImage from "next/image";
 
-// const defaultPlaceholder =
-// 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkEBKqBwAA4wClZCmZjAAAAABJRU5ErkJggg==";
-
 const defaultPlaceholder =
 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkEBL6DwABYwElBoU80gAAAABJRU5ErkJggg==";
 
-const Img = ({ src, alt, data, width, height, sizes, loading, ...props }) => (
+const Img = ({ src, alt = '', data, width, height, sizes, loading, ...props }) => (
 	<NextImage
 		src={data?.asset.url || src}
 		alt={data?.asset.altText || alt}
