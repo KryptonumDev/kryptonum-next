e44b377dc982f7935be3ca395a14ff24b5af0355
Shembou <92935278+Shembou@users.noreply.github.com>
2023-12-11 16:53:21 +0100

    scrollLock fix on nav

--- src/components/atoms/NavLink/index.js
+++ src/components/atoms/NavLink/index.js
@@ -3,12 +3,13 @@
 import Link from "next/link";
 
 const NavLink = ({ href, className, children, ...props }) => {
-
-  const handleNavLinks = () => {
-    const nav = document.querySelector('nav.nav');
-    nav.removeAttribute("data-tab");
-		nav.setAttribute("aria-expanded",false);
-  }
+	const handleNavLinks = () => {
+		const nav = document.querySelector("nav.nav");
+		nav.removeAttribute("data-tab");
+		nav.setAttribute("aria-expanded", false);
+		const body = document.querySelector("body");
+		body.classList.remove("scrollLock");
+	};
 
 	return (
 		<Link
