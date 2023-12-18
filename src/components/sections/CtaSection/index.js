822ec1a67fbdabaedd3dc8b8d546a9654cef225d
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2023-12-18 14:19:56 +0100

    fix resposivness of CtaSection

--- src/components/sections/CtaSection/index.js
+++ src/components/sections/CtaSection/index.js
@@ -1,22 +1,17 @@
-import Img from '@/components/atoms/Img';
-import Button from '../../atoms/Button';
+import Img from "@/components/atoms/Img";
+import Button from "../../atoms/Button";
 import DecorativeHeading from "../../atoms/DecorativeHeading";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-const CtaSection = ({ data: { 
-	img,
-	cta,
-	heading
- } 
-}) => {
-	return (
-		<section className={styles.wrapper}>
-			<header className={styles.grid}>
-				<DecorativeHeading type="h3">{heading}</DecorativeHeading>
-				<Button data={cta}/>
-			</header>
-			<Img data={img} className={styles.img} sizes="(max-width: 500px) 100vw, (max-width: 1199px) 70vw, 50vw"/>
-		</section>
-	);
+const CtaSection = ({ data: { img, cta, heading } }) => {
+  return (
+    <section className={styles.wrapper}>
+      <header className={styles.grid}>
+        <DecorativeHeading type="h2">{heading}</DecorativeHeading>
+        <Button data={cta} />
+      </header>
+      <Img data={img} className={styles.img} sizes="(max-width: 500px) 100vw, (max-width: 1199px) 70vw, 50vw" />
+    </section>
+  );
 };
 export default CtaSection;
