6f37ba1b9b234b1518699d0ea8bf1680bee0322e
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2023-12-18 11:19:19 +0100

    remove duplicated SliderArrowRight

--- src/components/sections/Slider/index.js
+++ src/components/sections/Slider/index.js
@@ -17,20 +17,3 @@ const Slider = ({ data }) => {
 	);
 };
 export default Slider;
-
-export const SliderArrowRight = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		fill="none"
-	>
-		<path
-			stroke="#EFF0F3"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth="2"
-			d="M5 12h14m0 0l-7-7m7 7l-7 7"
-		></path>
-	</svg>
-);
--- src/global/Seo.js
+++ src/global/Seo.js
@@ -4,76 +4,76 @@ export const domain = "https://kryptonum.eu";
 export const locale = "pl_PL";
 
 const SEO = async ({
-	pagination,
-	author,
-	date,
-	title,
-	description,
-	url,
-	ogImage,
-	children,
-	...props
+  pagination,
+  author,
+  date,
+  title,
+  description,
+  url,
+  ogImage,
+  children,
+  ...props
 }) => {
-	const {
-		body: {
-			data: {
-				global: {
-					globalSeo: { og_Img },
-				},
-			},
-		},
-	} = await getData();
+  const {
+    body: {
+      data: {
+        global: {
+          globalSeo: { og_Img },
+        },
+      },
+    },
+  } = await getData();
 
-	const seo = {
-		title: title || "Kryptonum - Agencja dla tych, którym zależy",
-		description: description || "",
-		url: url ? url + (pagination ? `/${pagination}` : "") : "",
-		ogImage: og_Img.asset.url,
-	};
+  const seo = {
+    title: title || "Kryptonum - Agencja dla tych, którym zależy",
+    description: description || "",
+    url: url ? url + (pagination ? `/${pagination}` : "") : "",
+    ogImage: og_Img.asset.url,
+  };
 
-	const metadata = {
-		robots: {
-			index: false,
-		},
-		metadataBase: new URL(domain),
-		title: seo.title,
-		description: seo.description,
-		alternates: {
-			canonical: seo.url,
-		},
-		openGraph: {
-			title: seo.title,
-			description: seo.description,
-			siteName: seo.title,
-			url: seo.url,
-			images: [
-				{
-					url: seo.ogImage,
-					width: 1200,
-					height: 630,
-				},
-			],
-			locale: locale,
-			type: "website",
-		},
-		...props
-	};
-	return metadata;
+  const metadata = {
+    robots: {
+      index: false,
+    },
+    metadataBase: new URL(domain),
+    title: seo.title,
+    description: seo.description,
+    alternates: {
+      canonical: seo.url,
+    },
+    openGraph: {
+      title: seo.title,
+      description: seo.description,
+      siteName: seo.title,
+      url: seo.url,
+      images: [
+        {
+          url: seo.ogImage,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: locale,
+      type: "website",
+    },
+    ...props,
+  };
+  return metadata;
 };
 
 const getData = async () => {
-	return await fetchData(`
-  query {
-  global: Global(id: "global") {
-      globalSeo {
-        og_Img {
-          asset {
-            url
-          }
-        }
-      }
-    }
-  }
+  return await fetchData(`
+		query {
+			global: Global(id: "global") {
+				globalSeo {
+					og_Img {
+						asset {
+							url
+						}
+					}
+				}
+			}
+		}
   `);
 };
 
