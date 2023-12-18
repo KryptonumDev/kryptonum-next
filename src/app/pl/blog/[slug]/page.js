397dcf3fe03c775cdc86ba8cf5860abda1be0fb2
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2023-12-18 13:43:16 +0100

    fix minor changes in BlogPost

--- src/app/pl/blog/[slug]/page.js
+++ src/app/pl/blog/[slug]/page.js
@@ -10,212 +10,212 @@ import { removeMarkdown } from "@/utils/functions";
 import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
-	const { allBlogEntries } = await paramsQuery();
-	return allBlogEntries.map((entry) => ({ slug: entry.slug.current }));
+  const { allBlogEntries } = await paramsQuery();
+  return allBlogEntries.map((entry) => ({
+    slug: entry.slug.current
+  }));
 }
 
 export default async function BlogSlugPage({ params }) {
-	const {
-		page: { title, subtitle, categories, _createdAt, img, contentRaw, author, seo, scrollToNext },
-		blogEntries,
-	} = await query(params.slug);
+  const {
+    page: { title, subtitle, categories, _createdAt, img, contentRaw, author, seo, scrollToNext },
+    blogEntries,
+  } = await query(params.slug);
 
-	const breadcrumbs = [
-		{
-			name: "Blog",
-			link: "/pl/blog",
-		},
-		{
-			name: title,
-			link: params.slug,
-		},
-	];
+  const breadcrumbs = [
+    {
+      name: "Blog",
+      link: "/pl/blog",
+    },
+    {
+      name: title,
+      link: params.slug,
+    },
+  ];
 
-	return (
-		<>
-			<main id="main">
-				<Breadcrumbs breadcrumbs={breadcrumbs} />
-				<EntryHero
-					title={title}
-					subtitle={subtitle}
-					categories={categories}
-					categorySlug="/pl/blog/kategoria/"
-					_createdAt={_createdAt}
-					img={img}
-				/>
-				<Content
-					contentRaw={contentRaw}
-					author={author}
-					share={seo}
-				/>
-				<LatestBlogEntries
-					exclude={params.slug}
-					data={blogEntries}
-				/>
-				<LatestCuriosityEntries />
-			</main>
-      {scrollToNext && <ScrollToNext data={scrollToNext}/>}
-		</>
-	);
+  return (
+    <>
+      <main id="main">
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+        <EntryHero
+          title={title}
+          subtitle={subtitle}
+          categories={categories}
+          categorySlug="/pl/blog/kategoria/"
+          _createdAt={_createdAt}
+          img={img}
+        />
+        <Content contentRaw={contentRaw} author={author} share={seo} />
+        <LatestBlogEntries exclude={params.slug} data={blogEntries} />
+        <LatestCuriosityEntries />
+      </main>
+      {scrollToNext && <ScrollToNext data={scrollToNext} />}
+    </>
+  );
 }
 
 export async function generateMetadata({ params: { slug } }) {
-	const data = await query(slug);
-	return SEO({
-		title: data?.page?.seo.title,
-		description: data?.page?.seo.description,
-		url: `/pl/blog/${slug}`,
-	});
+  const {
+    page: {
+      seo,
+      img: {
+        asset: { url: ogImage },
+      },
+    },
+  } = await query(slug);
+  return SEO({
+    title: seo?.title,
+    description: seo?.description,
+    url: `/pl/blog/${slug}`,
+    ogImage: `${ogImage}?w=1200`,
+  });
 }
 
 const query = async (slug) => {
-	const {
-		body: { data },
-	} = await fetchData(
-		`
-  query($slug: String!) {
-  page: allBlogEntries(
-    sort: { _createdAt: DESC }
-    where: {slug: {current: {eq: $slug}}}
-  ) {
-    title
-    subtitle
-    slug {
-      current
-    }
-    author {
-      name
-      slug {
-        current
-      }
-      img {
-        asset {
-          altText
-          url
-          metadata {
-            lqip
-            dimensions {
-              height
-              width
+  const {
+    body: { data },
+  } = await fetchData(
+    /* GraphQL */ `
+      query ($slug: String!) {
+        page: allBlogEntries(sort: { _createdAt: DESC }, where: { slug: { current: { eq: $slug } } }) {
+          title
+          subtitle
+          slug {
+            current
+          }
+          author {
+            name
+            slug {
+              current
+            }
+            img {
+              asset {
+                altText
+                url
+                metadata {
+                  lqip
+                  dimensions {
+                    height
+                    width
+                  }
+                }
+              }
+            }
+          }
+          categories {
+            name
+            slug {
+              current
+            }
+          }
+          img {
+            asset {
+              altText
+              url
+              metadata {
+                lqip
+                dimensions {
+                  height
+                  width
+                }
+              }
+            }
+          }
+          contentRaw
+          _createdAt
+          # SEO
+          seo {
+            title
+            description
+          }
+        }
+        blogEntries: allBlogEntries(limit: 4, sort: { _createdAt: DESC }) {
+          title
+          subtitle
+          slug {
+            current
+          }
+          author {
+            name
+            slug {
+              current
+            }
+            img {
+              asset {
+                altText
+                url
+                metadata {
+                  lqip
+                  dimensions {
+                    height
+                    width
+                  }
+                }
+              }
+            }
+          }
+          categories {
+            name
+            slug {
+              current
+            }
+          }
+          _createdAt
+          contentRaw
+          img {
+            asset {
+              altText
+              url
+              metadata {
+                lqip
+                dimensions {
+                  height
+                  width
+                }
+              }
             }
           }
         }
-      }
-    }
-    categories {
-      name
-      slug {
-        current
-      }
-    }
-    img {
-      asset {
-        altText
-        url
-        metadata {
-          lqip
-          dimensions {
-            height
-            width
+        scrollToNext_BlogPost: allBlogEntries(sort: { _createdAt: ASC }) {
+          title
+          slug {
+            current
           }
         }
       }
+    `,
+    {
+      slug,
     }
-    contentRaw
-    _createdAt
-    # SEO
-    seo {
-      title
-      description
-    }
+  );
+  if (slug) {
+    data.page ? (data.page = data.page[0]) : notFound();
+    slug !== data.page?.slug.current && notFound();
   }
-  blogEntries: allBlogEntries(limit: 4, sort: { _createdAt: DESC }) {
-    title
-    subtitle
-    slug {
-      current
-    }
-    author {
-      name
-      slug {
-        current
-      }
-      img {
-        asset {
-          altText
-          url
-          metadata {
-            lqip
-            dimensions {
-              height
-              width
-            }
-          }
-        }
-      }
-    }
-    categories {
-      name
-      slug {
-        current
-      }
-    }
-    _createdAt
-    contentRaw
-    img {
-      asset {
-        altText
-        url
-        metadata {
-          lqip
-          dimensions {
-            height
-            width
-          }
-        }
-      }
-    }
-  }
-  scrollToNext_BlogPost: allBlogEntries(sort: { _createdAt: ASC }) {
-    title
-    slug {
-      current
-    }
-  }
-}
-  `,
-		{
-			slug,
-		},
-	);
-	if (slug) {
-		data.page ? (data.page = data.page[0]) : notFound();
-		slug !== data.page?.slug.current && notFound();
-	}
   const blogPosts = data?.scrollToNext_BlogPost;
-  const currentIndex = blogPosts.findIndex(item => item.slug.current === data.page.slug.current);
+  const currentIndex = blogPosts.findIndex((item) => item.slug.current === data.page.slug.current);
   const nextPost = currentIndex !== -1 && currentIndex < blogPosts.length - 1 ? blogPosts[currentIndex + 1] : null;
   if (nextPost) {
     data.page.scrollToNext = {
-      paragraph: '**Scrolluj**, by przejść do następnego artykułu',
-      title: 'Następny post:',
-      link: { text: removeMarkdown(nextPost.title), href: `/pl/blog/${nextPost.slug.current}` }
+      paragraph: "**Scrolluj**, by przejść do następnego artykułu",
+      title: "Następny post:",
+      link: { text: removeMarkdown(nextPost.title), href: `/pl/blog/${nextPost.slug.current}` },
     };
   }
-	return data;
+  return data;
 };
 
 const paramsQuery = async () => {
-	const {
-		body: { data },
-	} = await fetchData(`
-  query{
-    allBlogEntries: allBlogEntries {
-      slug
-      {
-        current
+  const {
+    body: { data },
+  } = await fetchData(/* GraphQL */ `
+    query {
+      allBlogEntries: allBlogEntries {
+        slug {
+          current
+        }
       }
     }
-}`);
-	return data;
+  `);
+  return data;
 };
--- src/components/atoms/Img.js
+++ src/components/atoms/Img.js
@@ -3,17 +3,7 @@ import NextImage from "next/image";
 const defaultPlaceholder =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkEBL6DwABYwElBoU80gAAAABJRU5ErkJggg==";
 
-const Img = ({
-  src,
-  alt = "",
-  data,
-  width,
-  height,
-  sizes,
-  loading,
-  priority,
-  ...props
-}) => (
+const Img = ({ src, alt = "", data, width, height, sizes, loading, priority, ...props }) => (
   <NextImage
     src={data?.asset.url || src}
     alt={data?.asset.altText || alt}
@@ -22,15 +12,13 @@ const Img = ({
     loading={loading}
     priority={priority}
     sizes={sizes}
-    blurDataURL={data?.asset.metadata?.lqip || defaultPlaceholder}
-    placeholder={
-      width > 40 ||
-      data?.asset.metadata.dimensions?.width > 40 ||
-      height > 40 ||
-      data?.asset.metadata.dimensions?.height > 40
-        ? "blur"
-        : "empty"
-    }
+    {...(
+      width || data?.asset.metadata.dimensions?.width > 40 ||
+      height || data?.asset.metadata.dimensions?.height > 40 && {
+        placeholder: "blur",
+        blurDataURL: data?.asset.metadata?.lqip || defaultPlaceholder,
+      }
+    )}
     {...props}
   />
 );
