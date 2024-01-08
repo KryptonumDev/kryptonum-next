7cd702be79a8bc5532a11418d0d82f3bf89db0d0
Shembou <92935278+Shembou@users.noreply.github.com>
2024-01-08 10:56:05 +0100

    temporary solution for incoming changes

--- src/components/sections/ProcessList/index.js
+++ src/components/sections/ProcessList/index.js
@@ -2,28 +2,46 @@ import Markdown from '@/components/atoms/Markdown';
 import styles from './styles.module.scss';
 import Img from '@/components/atoms/Img';
 import Button from '@/components/atoms/Button';
+import CtaSectionPill from '../CtaSectionPill';
 
 const ProcessList = ({ data: { ProcessList } }) => {
+  let ctaSectionPillCounter = 0;
+
   return (
     <section className={styles.processList}>
-      {ProcessList.map(({ heading, subheading, img, paragraph, callToAction }, i) => (
+      {ProcessList.map(({ heading, subheading, img, paragraph, callToAction, callToActionSectionPill }, i) => (
         <div
           className={styles.process}
           key={i}
         >
-          <div className={styles.heading}>
-            <p className={`${styles.numerator} personBorder`}>{i + 1}</p>
-            <Markdown.h2>{heading}</Markdown.h2>
-          </div>
-          <Markdown className={styles.subheading}>{subheading}</Markdown>
-          <Img
-            data={img}
-            className={styles.img}
-          />
-          <div className={styles.blocks}>
-            <Markdown>{paragraph}</Markdown>
-            { callToAction?.text && <Button data={callToAction} className={styles.button}/>}
-          </div>
+          {heading && (
+            <>
+              <div className={styles.heading}>
+                <p className={`${styles.numerator} personBorder`}>{i + 1 - ctaSectionPillCounter}</p>
+                <Markdown.h2>{heading}</Markdown.h2>
+              </div>
+              <Markdown className={styles.subheading}>{subheading}</Markdown>
+              <Img
+                data={img}
+                className={styles.img}
+              />
+              <div className={styles.blocks}>
+                <Markdown>{paragraph}</Markdown>
+                {callToAction?.text && (
+                  <Button
+                    data={callToAction}
+                    className={styles.button}
+                  />
+                )}
+              </div>
+            </>
+          )}
+          {callToActionSectionPill?.heading && (
+            <>
+              <CtaSectionPill data={callToActionSectionPill} />
+              {ctaSectionPillCounter++}
+            </>
+          )}
         </div>
       ))}
     </section>
