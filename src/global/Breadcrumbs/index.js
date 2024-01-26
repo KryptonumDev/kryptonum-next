4fee16c0864418f34af41f07c4ff653e2dfd9b10
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-26 10:51:42 +0100

    changed a to Next Link in Breadcrumbs

--- src/global/Breadcrumbs/index.js
+++ src/global/Breadcrumbs/index.js
@@ -1,8 +1,8 @@
-import { removeMarkdown } from "@/utils/functions";
+import { Fragment } from "react";
 import Link from "next/link";
 import Script from "next/script";
-import { Fragment } from "react";
 import styles from "./styles.module.scss";
+import { removeMarkdown } from "@/utils/functions";
 
 const Icon = () => (
   <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -33,38 +33,37 @@ const createBreadcrumbs = (breadCrumbs) => {
 }
 
 const Breadcrumbs = ({portfolio, breadcrumbs}) => {
-	
   if (breadcrumbs?.length < 1 || !breadcrumbs) {
     return null;
   }
-
   const breadCrumbsItems = createBreadcrumbs(breadcrumbs);
+  
   return (
     <nav className={portfolio ? `${styles.portfolio} ${styles.wrapper} breadcrumb` : `${styles.wrapper} breadcrumb`}>
-    {breadCrumbsItems.length > 1 && (
-      <Script type="application/ld+json" id="breadcrumbsScript">
-        {JSON.stringify({
-          "@context": "https://schema.org",
-          "@type": "BreadcrumbList",
-          "itemListElement": breadCrumbsItems
-        })}
-      </Script>
-    )}
-    <ul>
-      <li><a href="/pl">Strona główna</a></li>
-      <li><Icon /></li>
-      {breadcrumbs?.map((el, index) => (
-        <Fragment key={index}>
-          {breadcrumbs.length - 1 !== index ? <>
-            <li><Link href={el.link}>{removeMarkdown(el.name)}</Link></li>
-            <li><Icon /></li>
-          </> :
-            <li>{removeMarkdown(el.name)}</li>
-          }
-        </Fragment>
-      ))}
-    </ul>
-  </nav>
+      {breadCrumbsItems.length > 1 && (
+        <Script type="application/ld+json" id="breadcrumbsScript">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": breadCrumbsItems
+          })}
+        </Script>
+      )}
+      <ul>
+        <li><Link href="/pl">Strona główna</Link></li>
+        <li><Icon /></li>
+        {breadcrumbs?.map((el, index) => (
+          <Fragment key={index}>
+            {breadcrumbs.length - 1 !== index ? <>
+              <li><Link href={el.link}>{removeMarkdown(el.name)}</Link></li>
+              <li><Icon /></li>
+            </> :
+              <li>{removeMarkdown(el.name)}</li>
+            }
+          </Fragment>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
